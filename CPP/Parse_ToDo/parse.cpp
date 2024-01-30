dac4f0303abcfbde61d6c915bd01a7505bcca566
Patryk Michalak <pmichalak16@gmail.com>
2024-01-30 17:20:53 +0100

    Continued worked on parsed version of Tasklist

--- CPP/Parse_ToDo/parse.cpp
+++ CPP/Parse_ToDo/parse.cpp
@@ -1,17 +1,60 @@
-#include<iostream>
-#include<sstream>
-#include<vector>
-#include<queue>
-#include<algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cstdint>
 
 #include "task.hpp"
 #include "states.hpp"
 
-bool is_number(const std::string &s) {
+void clear_queue(std::queue<std::string>& queue_in)
+{
+    while (!queue_in.empty())
+    {
+        queue_in.pop();
+    }
+    return;
+}
+
+bool is_number(const std::string &s) 
+{
   return !s.empty() && std::all_of(s.begin(), s.end(), ::isdigit);
 }
 
-bool process(std::string line, std::vector<Task> tasklist)
+void change_title(std::vector<Task>& tasklist, std::queue<std::string>& words,const uint32_t& index)
+{
+    if(index > tasklist.size()) // Size Check
+        {
+            std::cout << "ERROR: Incorrect index - not enough elements in tasklist" << std::endl;
+            while (!words.empty())  words.pop();
+            return;
+        }
+    std::string title = {};
+    while (!words.empty())
+    {
+        title += words.front() + " ";
+        words.pop();
+    }
+    
+    tasklist[index].modify_title(title);
+    return;
+}
+
+void delete_task(std::vector<Task>& tasklist, uint32_t index)
+{
+    if(tasklist.size() < index)
+        {
+            std::cout << "Tasklist doesnt have this many tasks!" << std::endl;
+            return;
+        }
+
+    tasklist.erase(tasklist.begin() + index);
+    tasklist.shrink_to_fit();
+    return;
+}
+
+bool process(std::string line, std::vector<Task>& tasklist)
 {
     std::stringstream stream_line {line};
     std::string temp;
@@ -30,15 +73,29 @@ bool process(std::string line, std::vector<Task> tasklist)
         case NEW:
         case SHOW:
         case MODIFY:
-            int count = stoi(words.front());
+            if (!is_number( words.front() )) 
+                {std::cout << "ERROR:Second argument not number!\n";return 0;}
+            uint32_t count = stoi(words.front());
             words.pop();
+            change_title(tasklist,words,count);
+            break;
         case DELETE:
+            if( !is_number(words.front()) )
+                {std::cout << "ERROR:Second argument not number!\n";return 0;}
+            uint32_t count = stoi(words.front());
+            clear_queue(words);
+            delete_task(tasklist,count);
+            break;
         case LOAD:
+            break;
         case SAVE:
+            break;
         case MARKED:
+            break;
         case HELP:
+            break;
         case EXIT:
-
+            return true;
 
         default:
             std::cout << "ERROR: Unknown command" << std::endl;
@@ -62,4 +119,6 @@ int main()
         }
     line.clear();
     }
+
+    return -1;
 }
